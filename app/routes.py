940063918a241da01fcdc5a6340f7ac2a57d6fb4
dtuckersuh <suh977@gmail.com>
2020-02-23 01:34:21 -0800

    bootstrapped and user page

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,25 @@
 # Routes module contains different URLs app implements
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
 from app import app
 from .summoner import Summoner
+from app.form import SearchForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
+# Homepage view
 def index():
-    user = Summoner('suh hyungwon')
-    champions = user.getTopFiveChamps()  # Dictionary
-    match_history = user.getMatchHistory()
-    return render_template('index.html', title='Home', user=user, champions=champions, match_history=match_history)
+    form = SearchForm()
+    if form.validate_on_submit():
+        name = form.username.data
+        # TODO: Handle errors and sanitize
+        return redirect(url_for('user', username=name))
+    else:
+        flash('All fields required')
+    return render_template('index.html', title='Home', form=form)
 
+@app.route('/user/<username>')
+def user(username):
+    user = Summoner(username)
+    return render_template('user.html', title='User', user=user)
 
 
