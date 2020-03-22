8001ed771ca67396eb4fda07682c27f3e80a15d4
dtuckersuh <suh977@gmail.com>
2020-03-21 18:06:58 -0700

    Match History Graph Skeleton

---
+++ app/static/graphs/match_history.js
@@ -0,0 +1,12 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['03/14','03/15', '03/16', '03/17', '03/18', '03/19', '03/20'],
+        datasets : [{
+            label: 'LP',
+            data : [64, 50, 74, 56, 45, 55, 78],
+            backgroundColor: "rgba(44, 144, 61, 1)"
+        }]
+    }
+});
