52f9f1d8308680505bd21f0fd48fbeec4784b8c7
dtuckersuh <suh977@gmail.com>
2020-04-07 23:43:05 -0700

    Redesign

--- app/league.py
+++ app/league.py
@@ -1,12 +1,17 @@
 from app import app
+import re
 
 class League:
 
     def __init__(self, summoner):
         self.summoner = summoner
-
+ 
     def by_summoner(self, region, summoner_id):
         stats = self.watcher.league.by_summoner(region, summoner_id)
-        return stats[0]
+        ranked_solo = None
+        for queueType in stats:
+            if queueType.get('queueType') == "RANKED_SOLO_5x5":
+                ranked_solo = queueType
+        return ranked_solo
 
     
